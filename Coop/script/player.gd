4846434f37636456b7c950fd59f936a3b8a02877
YueoKanaski <124761460+YueoKanaski@users.noreply.github.com>
2025-03-31 19:14:09 +0400

    Nouvelle version
    Ajout des enemeie fonctionnel , écran titre , fin  et amélioration de la horde

--- Coop/script/player.gd
+++ Coop/script/player.gd
@@ -1,4 +1,5 @@
 extends CharacterBody2D
+
 @onready var attack_area: Area2D = $AttackArea
 @onready var collision_shape_2d: CollisionShape2D = $CollisionShape2D
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
@@ -61,5 +62,6 @@ func _physics_process(delta: float) -> void:
 	move_and_slide() 
 
 func _on_attack_area_body_entered(body: Node2D) -> void:
-	if attacking and body.is_in_group("enemies") and body.has_method("take_damage"):
+	if attacking and body.has_method("take_damage"):
+		print("hit")
 		body.take_damage()
---
+++ Coop/script/title_screen.gd
@@ -0,0 +1,20 @@
+extends Control
+
+
+
+func _on_play_pressed() -> void:
+	get_tree().change_scene_to_file("res://scene/Game.tscn")
+	
+	
+
+
+func _on_play_harmode_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_option_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_quit_pressed() -> void:
+	get_tree().quit()
